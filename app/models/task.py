2642eb47891446316f976a37c09654c03dab14ba
Jhonatan Sousa <jhonatan-ce@hotmail.com>
2025-07-24 11:41:46 -0300

    feat: Model Task

---
+++ app/models/task.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
+from app.database.base import Base
+
+class Task(Base):
+    __tablename__ = "tasks"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=False)
+    description = Column(String, index=True, nullable=False)
+    completed = Column(String, index=True, default="Pendente")
+    user_id = Column(Integer, ForeignKey("users.id"), index=True)
+
+    user = relationship("User", backref="tasks")
